c977b71e5cc70ea1b8d9d1e969c413f16aea231f
Alexander Antonison <adantonison@gmail.com>
2025-06-02 11:46:14 +0100

    Update model to sum up release, rating, and genre

--- dbt/models/marts/sample_model.sql
+++ dbt/models/marts/sample_model.sql
@@ -1,2 +1,10 @@
-SELECT *
+SELECT
+    release_year,
+    mpaa_rating,
+    major_genres,
+    sum(worldwide_gross)
 FROM {{ ref('stg_movies') }}
+GROUP BY
+    release_year,
+    mpaa_rating,
+    major_genres
