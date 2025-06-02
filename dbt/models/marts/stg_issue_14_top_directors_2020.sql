aebc90a1b8369b8f43fb0687899aeebea2de4b04
Morgan Rennie <morgan.a.rennie@gmail.com>
2025-06-02 12:03:52 +0100

    fixed sql model

--- dbt/models/marts/stg_issue_14_top_directors_2020.sql
+++ dbt/models/marts/stg_issue_14_top_directors_2020.sql
@@ -2,7 +2,7 @@ select
     director,
     avg(rotten_tomatoes_rating) as avg_tomatoes_rating
 from {{ ref('stg_movies')}}
-where release_date >= '2000-01-01' and release_date <= '2011-01-01'
+where release_year >= '2000' and release_year <= '2011'
 group by director
 order by avg_tomatoes_rating desc
 limit 10
